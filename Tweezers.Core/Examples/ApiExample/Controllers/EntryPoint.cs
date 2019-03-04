dbe6d035e810ec16aee9d2e708ac47697e9fabfa
Nissan Cohen <nissanc6@gmail.com>
2019-03-04 14:33:24 +0200

    ApiExample: Enhancing Demo

--- Tweezers.Core/Examples/ApiExample/Controllers/EntryPoint.cs
+++ Tweezers.Core/Examples/ApiExample/Controllers/EntryPoint.cs
@@ -11,14 +11,25 @@ namespace ApiExample.Controllers
             Description = "Hello! Welcome to the tweezers example app";
 
             PersonController pc = new PersonController();
-            pc.Post(new Person() { Name = "Moshe", CatName = "Shmil", Gender = Gender.Male });
-            pc.Post(new Person() { Name = "Yakov", CatName = "Simon", Gender = Gender.Male });
-            pc.Post(new Person() { Name = "Hatul", CatName = "Adam", Gender = Gender.Unknown });
+            pc.Post(new Person() { Name = "Moshe", Gender = Gender.Male });
+            pc.Post(new Person() { Name = "Yakov", Gender = Gender.Male });
+            pc.Post(new Person() { Name = "Miki", Gender = Gender.Unknown });
 
-            ShoeBrandController prc = new ShoeBrandController();
-            prc.Post(new ShoeBrand() { Id = "1234", Name = "Abibas" });
-            prc.Post(new ShoeBrand() { Id = "2345", Name = "Buma" });
-            prc.Post(new ShoeBrand() { Id = "3456", Name = "Berez" });
+            LaptopController lc = new LaptopController();
+            lc.Post(new Laptop() {BrandName = "Asus", Name = "ZenBook z6", Price = 6000.00, CpuModel = "i7 8750u", Memory = "16GB", Disk = "512GB SSD"});
+            lc.Post(new Laptop() {BrandName = "Lenovo", Name = "Yoga 3", Price = 5560.00, CpuModel = "i7 7600u", Memory = "8GB", Disk = "256GB SSD"});
+            lc.Post(new Laptop() {BrandName = "Dell", Name = "Latitude 5480", Price = 7600.00, CpuModel = "i7 7820HQ", Memory = "16GB", Disk = "512GB SSD"});
+
+            SmartPhoneController sc = new SmartPhoneController();
+            sc.Post(new SmartPhone() { BrandName = "Samsung", Name = "Galaxy s10", Price = 4000.00, CpuModel = "Snapdragon 840", Memory = "6GB", Disk = "64GB" });
+            sc.Post(new SmartPhone() { BrandName = "Google", Name = "Pixel 3", Price = 3400.00, CpuModel = "Snapdragon 835", Memory = "4GB", Disk = "128GB" });
+            sc.Post(new SmartPhone() { BrandName = "Apple", Name = "iPhone XS", Price = 5600.00, CpuModel = "ARM A12", Memory = "4GB", Disk = "512GB" });
+
+            PeripheralController prc = new PeripheralController();
+            prc.Post(new Peripheral() {BrandName = "Corsair", Name = "K70", PeripheralType = PeripheralType.Keyboard, Price = 360});
+            prc.Post(new Peripheral() {BrandName = "Razer", Name = "Kraken 7.1", PeripheralType = PeripheralType.Headphones, Price = 360});
+            prc.Post(new Peripheral() {BrandName = "Microsoft", Name = "XBox Controller", PeripheralType = PeripheralType.Keyboard, Price = 400});
+            prc.Post(new Peripheral() {BrandName = "Logitech", Name = "Marathon Mouse", PeripheralType = PeripheralType.Mouse, Price = 160});
         }
     }
 }
---
+++ Tweezers.Core/Examples/ApiExample/DataHolders/Laptop.cs
@@ -0,0 +1,30 @@
+﻿using Tweezers.Discoveries.Attributes;
+using Tweezers.Discoveries.Rbac;
+
+namespace ApiExample.DataHolders
+{
+    [TweezersEntity("Laptop", AllowedActions = TweezersAllowedActions.Admin, IconName = "laptop")]
+    public class Laptop
+    {
+        [TweezersId]
+        public string Id { get; set; }
+
+        [TweezersField("Brand Name")]
+        public string BrandName { get; set; }
+
+        [TweezersField("Model")]
+        public string Name { get; set; }
+
+        [TweezersField("CPU Model")]
+        public string CpuModel { get; set; }
+
+        [TweezersField("Memory")]
+        public string Memory { get; set; }
+
+        [TweezersField("Disk")]
+        public string Disk { get; set; }
+
+        [TweezersField("Price")]
+        public double Price { get; set; }
+    }
+}
