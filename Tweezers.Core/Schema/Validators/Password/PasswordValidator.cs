e14f5a1d0b6bd3b504d776b62860ef1a76c7273f
Nissan Cohen <nissanc6@gmail.com>
2019-05-10 10:48:10 +0300

    Users: Aligning for UI

---
+++ Tweezers.Core/Schema/Validators/Password/PasswordValidator.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using Tweezers.Schema.DataHolders;
+
+namespace Tweezers.Schema.Validators.Password
+{
+    public sealed class PasswordValidator : IValidator
+    {
+        private PasswordValidator()
+        {
+
+        }
+
+        public static PasswordValidator Create()
+        {
+            return new PasswordValidator();
+        }
+
+        public TweezersValidationResult Validate(string fieldName, dynamic value)
+        {
+            string passwordStr = value.ToString();
+            if (string.IsNullOrWhiteSpace(passwordStr))
+            {
+                return TweezersValidationResult.Reject("empty password");
+            }
+
+            bool hasCapitalLetter = passwordStr.Any(char.IsUpper);
+            bool hasLowerLetter = passwordStr.Any(char.IsLower);
+            bool hasDigit = passwordStr.Any(char.IsNumber);
+
+            if (hasCapitalLetter && hasLowerLetter && hasDigit)
+            {
+                return TweezersValidationResult.Accept();
+            }
+
+            return TweezersValidationResult.Reject("invalid password, must contain at least one capital letter, one lower letter and one number");
+        }
+
+        public string Name => "NotNull";
+    }
+}
