371baa3a233b71c08d186b099aaade68394fe7d7
Nissan Cohen <nissan@hysolate.com>
2019-07-01 21:32:49 +0300

    MongoDB: Adding basic connector

---
+++ Tweezers.Core/DBConnector/DBConnectionDetails.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable InconsistentNaming
+
+namespace Tweezers.DBConnector
+{
+    public sealed class DBConnectionDetails
+    {
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public string DBName { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
