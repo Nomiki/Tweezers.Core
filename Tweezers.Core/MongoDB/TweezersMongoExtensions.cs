3dc2cd548043ec6175695b7653664e79b9df04c5
Nissan Cohen <nissanc6@gmail.com>
2019-07-26 14:34:59 +0300

    Tweezers.Core: Adding dbConnector to json
    Fixing MongoDBConnector, closes #42

---
+++ Tweezers.Core/MongoDB/TweezersMongoExtensions.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using Newtonsoft.Json.Linq;
+
+namespace Tweezers.MongoDB
+{
+    internal static class TweezersMongoExtensions
+    {
+        public static JObject ToJObject(this BsonDocument bson)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(bson.ToJson());
+        }
+
+        public static BsonDocument ToBsonDocument(this JObject jObject)
+        {
+            return BsonDocument.Parse(Newtonsoft.Json.JsonConvert.SerializeObject(jObject));
+        }
+    }
+}
